fe41a108c014be6f193242b049bf09287173f674
grisha2109 <FilinGD@yandex.ru>
2026-01-04 11:47:02 +0300

    Сделал коммит. Создал список для хранения объектов. Добавил 5(пока что) объектов мусора. Нарисовал мусор, пока что в виде жёлтых кружочков. Добавил функцию on_update для движения объектов. Создал простые столкновения.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import arcade
+import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -11,6 +12,9 @@ class GameWindow(arcade.Window):
         self.player_y = 50
         self.score = 0
         arcade.set_background_color(arcade.color.BLUEBERRY)
+        self.trash = []
+        for i in range(5):
+            self.trash.append({'x': random.randint(50, SCREEN_HEIGHT - 50), 'y': SCREEN_WIDTH - 50, 'speed': random.uniform(2, 4), 'size': random.randint(10, 20)})
 
     def on_draw(self):
         self.clear()
@@ -24,6 +28,10 @@ class GameWindow(arcade.Window):
         arcade.draw_text(f"Ваш счёт: {self.score}", SCREEN_WIDTH - 200, SCREEN_HEIGHT - 40, arcade.color.WHITE_SMOKE, 20)
         arcade.draw_text(f"Управление: <- ->", 10, SCREEN_HEIGHT - 40, arcade.color.WHITE_SMOKE, 20)
 
+        for j in self.trash:
+            arcade.draw_circle_filled(j['x'], j['y'], j['size'], arcade.color.YELLOW_ROSE)
+
+
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
             self.player_x -= 10
@@ -36,6 +44,19 @@ class GameWindow(arcade.Window):
         if self.player_x > SCREEN_WIDTH - 25:
             self.player_x = SCREEN_WIDTH - 25
 
+    def on_update(self, delta_time):
+        for i in self.trash:
+            i['y'] -= i['speed']
+            if i['y'] <= - i['size']:
+                i['y'] = SCREEN_HEIGHT + i['size']
+                i['x'] = random.randint(50, SCREEN_WIDTH - 50)
+
+            distance_x = abs(self.player_x - i['x'])
+            distance_y = abs(self.player_y - i['y'])
+            if distance_x < 35 and distance_y < 35:
+                self.score += 10
+                i['y'] = SCREEN_HEIGHT + i['size']
+                i['x'] = random.randint(50, SCREEN_WIDTH - 50)
 
 def main():
     window = GameWindow()
